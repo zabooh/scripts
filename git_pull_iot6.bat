767cdf2d07610f81ce780539a0d8ac26180d1767
martin <martin.ruppert@microchip.com>
2019-05-31 12:25:33 +0200

    scritps for lab sources and manuals

---
+++ git_pull_iot6.bat
@@ -0,0 +1,23 @@
+cd ../lab1
+git reset --hard HEAD
+git clean -f -d
+git checkout master
+git fetch origin master
+git reset --hard origin/master
+git pull 
+
+cd ../lab2
+git reset --hard HEAD
+git clean -f -d
+git checkout master
+git fetch origin master
+git reset --hard origin/master
+git pull
+
+cd ../lab3
+git reset --hard HEAD
+git clean -f -d
+git checkout master
+git fetch origin master
+git reset --hard origin/master
+git pull
---
+++ git_reset_iot6.bat
@@ -0,0 +1,20 @@
+cd ../lab1
+git reset --hard HEAD
+git clean -f -d
+git checkout master
+git fetch origin master
+git reset --hard origin/master
+
+cd ../lab2
+git reset --hard HEAD
+git clean -f -d
+git checkout master
+git fetch origin master
+git reset --hard origin/master
+
+cd ../lab3
+git reset --hard HEAD
+git clean -f -d
+git checkout master
+git fetch origin master
+git reset --hard origin/master
